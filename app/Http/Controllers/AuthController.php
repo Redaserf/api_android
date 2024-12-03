d6ac763ef51b9b9c7b43051db40159ed7ff5fe20
Redaserf <113316682+Redaserf@users.noreply.github.com>
2024-12-03 13:26:19 -0600

    quite jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Password;
-use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
