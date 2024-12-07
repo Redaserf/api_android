8132632328f5bd268c7b4a82bf406801f6788283
Redaserf <113316682+Redaserf@users.noreply.github.com>
2024-12-06 21:08:15 -0600

    Solo para reflejar cambios en el servidor

--- app/Http/Controllers/BicicletaController.php
+++ app/Http/Controllers/BicicletaController.php
@@ -152,7 +152,7 @@ class BicicletaController extends Controller
         //
         $validaciones = Validator::make($request->all(), [
             'nombre' => 'string|max: 60',
-            'imagen' => 'file|image'
+            'imagen' => 'file|image|max:2048'
         ], [
             'nombre.required' => 'El nombre es un campo obligatorio',
             'nombre.string' => 'El nombre debe ser de tipo string',
@@ -162,6 +162,8 @@ class BicicletaController extends Controller
             'imagen.file' => 'La imagen debe ser un archivo',
             'imagen.image' => 'La imagen debe ser una imagen',
             'imagen.mimes' =>  'La imagen debe ser de tipo png o jpg',
+            'imagen.max' => 'La imagen debe pesar menos de 2 megabytes'
+
 
         ]);
 
